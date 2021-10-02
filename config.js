ae3cb634a5a688c6c7bf8cc7f5cfe57844e1050f
PHUCLUVU <91784049+PHUCLUVU@users.noreply.github.com>
2021-10-02 18:25:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: " ĐỪNG CƯỜI NHA,TUI NGẠI:(((",
+    introTitle: ' THẢO MY SO YUMMY',
+    introDesc: `Những điều 
+                 khiến tui 
+                 tò mò
+                 bấy lâu nay :(((`,
+    btnIntro: ' ẤN DÔ ĐÂY NÈ><',
+    title: ' ẤN TƯỢNG ĐẦU TIÊN CỦA BÀ VỀ TUI LÀ GÌ VẬYYYY 🥰',
+    desc: ' :> CÓ PHẢI LÀ THẤY TUI ĐẸP TRAI ĐÚNG HONG?',
+    btnYes: ' QUÁ ĐÚNG RỒI ,U LÀ TRỪI',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích nói chuyện với mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Chắc là do tôi với bạn có duyên với nhau óooooo',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘' 
+    messDesc: 'nào dô đây đi chơi với tui nhaaaaaaa,'
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
